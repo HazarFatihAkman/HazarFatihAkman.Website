18ecd5fff2c943674a90eb3a8d40e7e1667c4f8b
HazarFatihAkman <akman.hazar.fatih@gmail.com>
2026-01-21 13:58:02 +0300

    font change

--- hazarfatihakman/src/app/page.tsx
+++ hazarfatihakman/src/app/page.tsx
@@ -5,16 +5,12 @@ export default function Home() {
   return (
     <div className={`${styles.page} ${styles.wolfsMouth}`}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+        <div className={styles.header}>
+          <h1>Hazar Fatih Akman</h1>
+          <h2>Software Engineer</h2>
+        </div>
+        <div></div>
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
